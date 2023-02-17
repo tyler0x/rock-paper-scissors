7dc5e4a711872bf01fffd9372d04f8f6590aed51
tyler0x <121253202+tyler0x@users.noreply.github.com>
2023-02-17 10:28:25 -0500

    Add playerSelection function

--- script.js
+++ script.js
@@ -5,13 +5,34 @@
     Randomly return rock or paper or scissors */
 
 function getComputerChoice () {
-    let choice = Math.floor((Math.random() * 3) +1);
-        if (choice === 1) {
+    let computerSelection = Math.floor((Math.random() * 3) +1);
+        if (computerSelection === 1) {
             return 'rock';
-        } else if (choice === 2) {
+        } else if (computerSelection === 2) {
             return 'paper';
         } else {
             return 'scissors';
-        }
-    console.log(choice)    
+        }  
 }
+
+/* Function to play a single round of Rock Paper Scissors
+
+    Create a playerSelection variable that prompts the player for a selection
+    The playerSelection parameter should be case-insensitive
+    Create a function that takes two parameters: playerSelection and computerSelection
+    Return a string that declares the winner of the round*/
+
+function playerSelection () {
+    let playerSelection = prompt('Rock, Paper, or Scissors? Your choice please: ', '').toLowerCase()
+    if (playerSelection === 'rock') {
+        return 1;
+    } else if (playerSelection === 'paper') {
+        return 2;
+    } else if (playerSelection === 'scissors') {
+        return 3;
+    } else {
+        return 'Please type in "Rock, Paper" or "Scissors" to play the game.'
+    }
+}
+
+
