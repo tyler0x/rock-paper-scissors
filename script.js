ea861c5c26c802ca160a4bd87b801bde5de409c1
tyler0x <121253202+tyler0x@users.noreply.github.com>
2023-02-17 11:47:56 -0500

    Add playRound function

--- script.js
+++ script.js
@@ -22,17 +22,21 @@ function getComputerChoice () {
     Create a function that takes two parameters: playerSelection and computerSelection
     Return a string that declares the winner of the round*/
 
-function playerSelection () {
-    let playerSelection = prompt('Rock, Paper, or Scissors? Your choice please: ', '').toLowerCase()
-    if (playerSelection === 'rock') {
-        return 1;
-    } else if (playerSelection === 'paper') {
-        return 2;
-    } else if (playerSelection === 'scissors') {
-        return 3;
+let computerSelection = getComputerChoice()
+let playerSelection = prompt('Rock, Paper, or Scissors? Your choice please: ', '').toLowerCase()
+
+function playRound (computerSelection, playerSelection) {
+    if (computerSelection === 'rock' && playerSelection === 'rock' || computerSelection === 'paper' && playerSelection === 'paper' || computerSelection === 'scissors' && playerSelection === 'scissors') {
+        return "It's a tie!"
+    } else if (computerSelection === 'paper' && playerSelection === 'rock' || computerSelection === 'rock' && playerSelection === 'scissors' || computerSelection === 'scissors' && playerSelection === 'paper') {
+        return 'The computer wins.'
+    } else if (computerSelection === 'paper' && playerSelection === 'scissors' || computerSelection === 'rock' && playerSelection === 'paper' || computerSelection === 'scissors' && playerSelection === 'rock') {
+        return 'You win!'
     } else {
-        return 'Please type in "Rock, Paper" or "Scissors" to play the game.'
+        return 'Please enter a valid choice to play the game: "Rock", "Paper", or "Scissors".'
     }
 }
 
-
+console.log(`The computer chose ${computerSelection}`);
+console.log(`You chose ${playerSelection}`);
+console.log (playRound(computerSelection, playerSelection));
